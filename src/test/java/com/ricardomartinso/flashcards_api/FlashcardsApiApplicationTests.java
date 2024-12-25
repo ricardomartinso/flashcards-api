1794d4730ef633db0f8974ea97541ad98d4fb4be
Ricardo Martins <ricardomartinso.dev@gmail.com>
2024-12-25 17:35:51 -0300

    first commit

---
+++ src/test/java/com/ricardomartinso/flashcards_api/FlashcardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardomartinso.flashcards_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
