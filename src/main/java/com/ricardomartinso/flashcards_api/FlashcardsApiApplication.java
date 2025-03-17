8afe74200cc670777e60f44d6a8667c7f99811cb
Ricardo Martins <ricardomartinso.dev@gmail.com>
2025-03-17 12:11:36 -0300

    config: adds modelmapper and swagger dependencies

--- src/main/java/com/ricardomartinso/flashcards_api/FlashcardsApiApplication.java
+++ src/main/java/com/ricardomartinso/flashcards_api/FlashcardsApiApplication.java
@@ -1,7 +1,9 @@
 package com.ricardomartinso.flashcards_api;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FlashcardsApiApplication {
@@ -10,4 +12,8 @@ public class FlashcardsApiApplication {
 		SpringApplication.run(FlashcardsApiApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
